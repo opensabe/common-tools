b70d7858ac2f9129ccfee2e48592cabfa4f6073c
heng.ma <heng.ma@sabegeek.com>
2024-07-16 17:21:00 +0800

    add tests

---
+++ common-utils/src/test/java/io/github/opensabe/common/utils/mapstruct/vo/ActivityDto.java
@@ -0,0 +1,11 @@
+package io.github.opensabe.common.utils.mapstruct.vo;
+
+import io.github.opensabe.mapstruct.core.Binding;
+
+@Binding
+public class ActivityDto extends Activity {
+
+    public ActivityDto(String name) {
+        super(name);
+    }
+}
