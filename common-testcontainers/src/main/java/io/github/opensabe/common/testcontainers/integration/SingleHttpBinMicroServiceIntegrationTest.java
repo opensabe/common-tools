244ee8d9a73f27afd262bb72a4a335369a747ffb
tech <cto@sb>
2024-08-29 21:57:56 +0800

    修正 thread-pool 的依赖，增加 httpbin 微服务单元测试

---
+++ common-testcontainers/src/main/java/io/github/opensabe/common/testcontainers/integration/SingleHttpBinMicroServiceIntegrationTest.java
@@ -0,0 +1,40 @@
+package io.github.opensabe.common.testcontainers.integration;
+
+import io.github.opensabe.common.testcontainers.CustomizedHttpBinContainer;
+import lombok.extern.log4j.Log4j2;
+import org.junit.jupiter.api.extension.BeforeAllCallback;
+import org.junit.jupiter.api.extension.ExtensionContext;
+import org.springframework.test.context.DynamicPropertyRegistry;
+
+/**
+ * 注册一个微服务
+ */
+@Log4j2
+public class SingleHttpBinMicroServiceIntegrationTest implements BeforeAllCallback, ExtensionContext.Store.CloseableResource {
+    public static CustomizedHttpBinContainer HTTP_BIN = new CustomizedHttpBinContainer();
+    public static final String HTTP_BIN_SERVICE_NAME = "httpbin";
+
+    @Override
+    public void beforeAll(ExtensionContext extensionContext) throws Exception {
+        //由于单元测试并发执行，这个只能启动一次，所以加锁
+        if (!HTTP_BIN.isRunning()) {
+            synchronized (SingleHttpBinMicroServiceIntegrationTest.class) {
+                if (!HTTP_BIN.isRunning()) {
+                    HTTP_BIN.start();
+                }
+            }
+        }
+    }
+
+    public static void setProperties(DynamicPropertyRegistry registry) {
+        registry.add(
+                "spring.cloud.discovery.client.simple.instances." + HTTP_BIN_SERVICE_NAME + "[0].uri",
+                () -> "http://localhost:" + HTTP_BIN.getHttpBinPort()
+        );
+    }
+
+    @Override
+    public void close() throws Throwable {
+        HTTP_BIN.stop();
+    }
+}
