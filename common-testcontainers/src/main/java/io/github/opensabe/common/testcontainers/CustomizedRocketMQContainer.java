b857eef0ba57ebf35e8d06e39d260038d5ca4c8c
houjianpo <houjianpo510@163.com>
2024-10-15 10:11:46 +0800

    调整一下修改listenPort和更新topic主题权限的顺序

--- common-testcontainers/src/main/java/io/github/opensabe/common/testcontainers/CustomizedRocketMQContainer.java
+++ common-testcontainers/src/main/java/io/github/opensabe/common/testcontainers/CustomizedRocketMQContainer.java
@@ -33,9 +33,9 @@ public class CustomizedRocketMQContainer extends GenericContainer<CustomizedRock
         List<String> updateBrokerConfigCommands = new ArrayList<>();
         // Update the brokerAddr and the clients can use the mapped address to connect the broker.
         updateBrokerConfigCommands.add(updateBrokerConfig("brokerIP1", getHost()));
-        updateBrokerConfigCommands.add(updateBrokerConfig("listenPort", getMappedPort(BROKER_PORT)));
         // Make the changes take effect immediately.
         updateBrokerConfigCommands.add(updateBrokerConfig("brokerPermission", defaultBrokerPermission));
+        updateBrokerConfigCommands.add(updateBrokerConfig("listenPort", getMappedPort(BROKER_PORT)));
 
         final String command = String.join(" && ", updateBrokerConfigCommands);
         ExecResult result = null;
