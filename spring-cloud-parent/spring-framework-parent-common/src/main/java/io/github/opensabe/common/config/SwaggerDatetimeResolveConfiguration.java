6ff8d33bc99241a2aa7f4cf9df4d131f6dd6b378
heng.ma <heng.ma@sabegeek.com>
2024-12-13 16:59:25 +0800

    优化代码，解决多个converter不生效的问题

--- spring-cloud-parent/spring-framework-parent-common/src/main/java/io/github/opensabe/common/config/SwaggerDatetimeResolveConfiguration.java
+++ spring-cloud-parent/spring-framework-parent-common/src/main/java/io/github/opensabe/common/config/SwaggerDatetimeResolveConfiguration.java
@@ -1,20 +1,23 @@
 package io.github.opensabe.common.config;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.JavaType;
 import com.fasterxml.jackson.databind.type.SimpleType;
 import io.swagger.v3.core.converter.AnnotatedType;
 import io.swagger.v3.core.converter.ModelConverter;
 import io.swagger.v3.core.converter.ModelConverterContext;
-import io.swagger.v3.core.jackson.ModelResolver;
-import io.swagger.v3.core.util.Json;
+import io.swagger.v3.core.util.AnnotationsUtils;
 import io.swagger.v3.core.util.PrimitiveType;
 import io.swagger.v3.oas.models.media.Schema;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
 
+import java.lang.annotation.Annotation;
 import java.lang.reflect.Type;
+import java.time.LocalDateTime;
 import java.util.Iterator;
 import java.util.Objects;
+import java.util.Optional;
 import java.util.Set;
 
 /**
@@ -23,10 +26,12 @@ import java.util.Set;
 public class SwaggerDatetimeResolveConfiguration {
 
 
+
+
     @Bean
     @ConditionalOnMissingBean
     public DateTimeModelConverter dateTimeModelConverter () {
-        return new DateTimeModelConverter(Json.mapper());
+        return new DateTimeModelConverter();
     }
 
     @Bean
@@ -57,67 +62,33 @@ public class SwaggerDatetimeResolveConfiguration {
     }
 
 
-    public static class DateTimeModelConverter extends ModelResolver {
 
-        public DateTimeModelConverter(ObjectMapper mapper) {
-            super(mapper);
+    public static class DateTimeModelConverter implements ModelConverter {
+        private final JavaType javaType;
+        private final Set<PrimitiveType> times;
+        public DateTimeModelConverter() {
+            this.javaType = SimpleType.constructUnsafe(LocalDateTime.class);
+            this.times = Set.of(PrimitiveType.DATE, PrimitiveType.DATE_TIME, PrimitiveType.PARTIAL_TIME);
         }
 
-        /**
-         * update the return result while the type matches
-         * {@link PrimitiveType#DATE}
-         * {@link PrimitiveType#DATE_TIME}
-         * {@link PrimitiveType#PARTIAL_TIME}x
-         * and replace it with {@link PrimitiveType#LONG}
-         */
         @Override
-        public Schema resolve(AnnotatedType annotatedType, ModelConverterContext context, Iterator<ModelConverter> next) {
-            if (isTime(annotatedType.getType())) {
+        public Schema resolve(AnnotatedType type, ModelConverterContext context, Iterator<ModelConverter> chain) {
+            if (isTime(type.getType())) {
                 Schema schema = PrimitiveType.LONG.createProperty();
-                resolveSchemaMembers(schema, annotatedType);
                 schema.setDefault(System.currentTimeMillis());
+                Optional.ofNullable(AnnotationsUtils.mergeSchemaAnnotations(type.getCtxAnnotations(), javaType))
+                        .map(resolvedSchemaOrArrayAnnotation ->
+                                resolvedSchemaOrArrayAnnotation instanceof io.swagger.v3.oas.annotations.media.ArraySchema arr?
+                                        arr.schema() :
+                                        (io.swagger.v3.oas.annotations.media.Schema) resolvedSchemaOrArrayAnnotation
+                        ).map(io.swagger.v3.oas.annotations.media.Schema::description)
+                        .ifPresent(schema::setDescription);
                 return schema;
             }
-            return super.resolve(annotatedType, context, next);
+            return chain.hasNext()? chain.next().resolve(type, context, chain) : null;
         }
 
-//
-//        @Override
-//        protected Object resolveDefaultValue(Annotated a, Annotation[] annotations, io.swagger.v3.oas.annotations.media.Schema schema) {
-//            if (Objects.isNull(schema)) {
-//                if (isTime(a.getRawType())) {
-//                    return System.currentTimeMillis();
-//                }
-//            }else {
-//                try {
-//                    ObjectMapper mapper = ObjectMapperFactory.buildStrictGenericObjectMapper();
-//                    mapper.enable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
-//                    return mapper.readTree(schema.defaultValue());
-//                } catch (IOException e) {
-//                    return schema.defaultValue();
-//                }
-//            }
-//            return super.resolveDefaultValue(a, annotations, schema);
-//        }
-//
-//        @Override
-//        protected Object resolveExample(Annotated a, Annotation[] annotations, io.swagger.v3.oas.annotations.media.Schema schema) {
-//            if (Objects.isNull(schema)) {
-//                if (isTime(a.getRawType())) {
-//                    return System.currentTimeMillis();
-//                }
-//            }else {
-//                if (!schema.example().isEmpty()) {
-//                    try {
-//                        ObjectMapper mapper = ObjectMapperFactory.buildStrictGenericObjectMapper();
-//                        return mapper.readTree(schema.example());
-//                    } catch (IOException e) {
-//                        return schema.example();
-//                    }
-//                }
-//            }
-//            return null;
-//        }
+
 
         private boolean isTime (Type rawType) {
             PrimitiveType primitiveType = PrimitiveType.fromName(rawType.getTypeName());
@@ -130,8 +101,5 @@ public class SwaggerDatetimeResolveConfiguration {
             }
             return primitiveType != null && times.contains(primitiveType);
         }
-
-        private Set<PrimitiveType> times = Set.of(PrimitiveType.DATE, PrimitiveType.DATE_TIME, PrimitiveType.PARTIAL_TIME);
     }
-
 }
