3e1d0f60fa2f6b991ca22ff748bcbc24a3ffb8ea
heng.ma <xinhui.niu@icloud.com>
2025-03-19 11:51:58 +0800

    Observation, IException不认为是失败，修改判断逻辑，包含ValidationException

--- spring-cloud-parent/spring-cloud-parent-web-common/src/main/java/io/github/opensabe/spring/cloud/parent/web/common/handler/ExceptionHandlerObservationAop.java
+++ spring-cloud-parent/spring-cloud-parent-web-common/src/main/java/io/github/opensabe/spring/cloud/parent/web/common/handler/ExceptionHandlerObservationAop.java
@@ -4,6 +4,7 @@ package io.github.opensabe.spring.cloud.parent.web.common.handler;
 import io.github.opensabe.common.observation.UnifiedObservationFactory;
 import io.github.opensabe.spring.cloud.parent.common.handler.IException;
 import io.micrometer.observation.Observation;
+import jakarta.validation.ValidationException;
 import lombok.extern.log4j.Log4j2;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
@@ -29,6 +30,10 @@ public class ExceptionHandlerObservationAop {
     @Order(Ordered.LOWEST_PRECEDENCE)
     @Around("exceptionHandler()")
     public Object aroundSocketIoAnnotationPointCut(ProceedingJoinPoint pjp) throws Throwable {
+        //IException是参数校验失败，这时候不认为请求失败了
+        if (pjp.getThis() instanceof GexceptionHandler) {
+            return pjp.proceed();
+        }
         //由于我们将异常已经 catch 住了，这样从 observation 看就没有异常，这不是我们想要的
         //我们想要的是就算 catch 住并且返回的是 200，也要在 observation 看到有异常
         Observation currentObservation = unifiedObservationFactory.getCurrentObservation();
@@ -36,8 +41,8 @@ public class ExceptionHandlerObservationAop {
             Object[] args = pjp.getArgs();
             if (args != null) {
                 for (Object arg : args) {
-                    //IException是参数校验失败，这时候不认为请求失败了
-                    if (arg instanceof Throwable throwable && !(arg instanceof IException)) {
+
+                    if (arg instanceof Throwable throwable) {
                         currentObservation.error(throwable);
                         break;
                     }
