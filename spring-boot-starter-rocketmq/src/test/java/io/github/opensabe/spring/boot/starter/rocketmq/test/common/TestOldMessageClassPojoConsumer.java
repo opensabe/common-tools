bf059a793023ed7e3492f24a6f834a970563acab
tech-leader <tech@sabegeek.com>
2025-09-11 18:25:36 +0800

    reformat v1 v2 mq message convertion

--- spring-boot-starter-rocketmq/src/test/java/io/github/opensabe/spring/boot/starter/rocketmq/test/common/TestOldMessageClassPojoConsumer.java
+++ spring-boot-starter-rocketmq/src/test/java/io/github/opensabe/spring/boot/starter/rocketmq/test/common/TestOldMessageClassPojoConsumer.java
@@ -15,16 +15,13 @@
  */
 package io.github.opensabe.spring.boot.starter.rocketmq.test.common;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.CopyOnWriteArrayList;
 
 import org.apache.rocketmq.spring.annotation.RocketMQMessageListener;
 
 import io.github.opensabe.common.entity.base.vo.BaseMQMessage;
-import io.github.opensabe.common.entity.base.vo.BaseMessage;
 import io.github.opensabe.common.utils.json.JsonUtil;
-import io.github.opensabe.spring.boot.starter.rocketmq.AbstractConsumer;
 import io.github.opensabe.spring.boot.starter.rocketmq.AbstractMQConsumer;
 import lombok.Getter;
 import lombok.extern.log4j.Log4j2;
@@ -37,13 +34,13 @@ import lombok.extern.log4j.Log4j2;
 public class TestOldMessageClassPojoConsumer extends AbstractMQConsumer {
 
     @Getter
-    private final List<MessageClassPojoWrapper> MessageClassPojoWrappers = new CopyOnWriteArrayList<>();
+    private final List<MessageClassPojoWrapper> messageClassPojoWrappers = new CopyOnWriteArrayList<>();
 
     @Override
     protected void onBaseMQMessage(BaseMQMessage baseMQMessage) {
         String data = baseMQMessage.getData();
         MessageClassPojo messageClassPojo = JsonUtil.parseObject(data, MessageClassPojo.class);
-        MessageClassPojoWrappers.forEach(MessageClassPojoWrapper -> {
+        messageClassPojoWrappers.forEach(MessageClassPojoWrapper -> {
             if (MessageClassPojoWrapper.getMessageClassPojo().equals(messageClassPojo)) {
                 log.info("Received old message: {}", messageClassPojo.getTimestamp());
                 MessageClassPojoWrapper.getCountDownLatch().countDown();
