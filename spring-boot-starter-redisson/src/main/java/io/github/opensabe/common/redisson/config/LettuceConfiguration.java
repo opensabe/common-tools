88409fcafaa20a21685cdf85a16a6857d8a23785
heng.ma <xinhui.niu@icloud.com>
2025-10-11 11:02:50 +0800

    修复observation

--- spring-boot-starter-redisson/src/main/java/io/github/opensabe/common/redisson/config/LettuceConfiguration.java
+++ spring-boot-starter-redisson/src/main/java/io/github/opensabe/common/redisson/config/LettuceConfiguration.java
@@ -1,19 +1,21 @@
 package io.github.opensabe.common.redisson.config;
 
+import io.github.opensabe.common.observation.UnifiedObservationFactory;
 import io.github.opensabe.common.secret.GlobalSecretManager;
 import io.lettuce.core.event.DefaultEventPublisherOptions;
 import io.lettuce.core.metrics.DefaultCommandLatencyCollector;
 import io.lettuce.core.metrics.DefaultCommandLatencyCollectorOptions;
 import io.lettuce.core.resource.DefaultClientResources;
+import io.micrometer.observation.Observation;
 import io.micrometer.observation.ObservationRegistry;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
 import org.springframework.data.redis.connection.lettuce.observability.MicrometerTracingAdapter;
+import org.springframework.data.util.Lazy;
 
 import java.time.Duration;
 
-@Configuration(proxyBeanMethods = false)
+//@Configuration(proxyBeanMethods = false)
 public class LettuceConfiguration {
     private static final String DEFAULT_APPLICATION_NAME = "application";
 
@@ -24,11 +26,11 @@ public class LettuceConfiguration {
      */
     @Bean
     public DefaultClientResources getDefaultClientResources(
-            ObservationRegistry observationRegistry, Environment environment
+            UnifiedObservationFactory unifiedObservationFactory, Environment environment
     ) {
         String applicationName = environment.getProperty("spring.application.name", DEFAULT_APPLICATION_NAME);
         DefaultClientResources build = DefaultClientResources.builder()
-                .tracing(new MicrometerTracingAdapter(observationRegistry, applicationName))
+                .tracing(new MicrometerTracingAdapter(new LazyObservationRegistry(unifiedObservationFactory), applicationName))
                 .commandLatencyRecorder(
                         new DefaultCommandLatencyCollector(
                                 // define collector
@@ -45,5 +47,40 @@ public class LettuceConfiguration {
     @Bean
     public RedisTemplateSecretFilter redisTemplateSecretFilter(GlobalSecretManager globalSecretManager) {
         return new RedisTemplateSecretFilter(globalSecretManager);
+    }
+
+
+    public static class LazyObservationRegistry implements ObservationRegistry {
+
+        private final Lazy<ObservationRegistry> observationRegistry;
+
+        public LazyObservationRegistry(UnifiedObservationFactory unifiedObservationFactory) {
+            this.observationRegistry = Lazy.of(() -> unifiedObservationFactory.getObservationRegistry());
+        }
+
+        @Override
+        public Observation getCurrentObservation() {
+            return observationRegistry.get().getCurrentObservation();
+        }
+
+        @Override
+        public Observation.Scope getCurrentObservationScope() {
+            return observationRegistry.get().getCurrentObservationScope();
+        }
+
+        @Override
+        public void setCurrentObservationScope(Observation.Scope current) {
+            observationRegistry.get().setCurrentObservationScope(current);
+        }
+
+        @Override
+        public ObservationConfig observationConfig() {
+            return observationRegistry.get().observationConfig();
+        }
+
+        @Override
+        public boolean isNoop() {
+            return observationRegistry.get().isNoop();
+        }
     }
 }
