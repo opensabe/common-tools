ca03894dbfd6249f2f0b0199c96ee73d366a9d30
maheng <heng.ma@sabegeek.com>
2026-01-08 16:56:02 +0800

    添加conditional

--- spring-boot-starter-otel-exporter/src/main/java/io/github/opensabe/spring/boot/starter/otel/exporter/configuration/CustomizedOtelConfiguration.java
+++ spring-boot-starter-otel-exporter/src/main/java/io/github/opensabe/spring/boot/starter/otel/exporter/configuration/CustomizedOtelConfiguration.java
@@ -15,12 +15,21 @@
  */
 package io.github.opensabe.spring.boot.starter.otel.exporter.configuration;
 
+import io.opentelemetry.api.common.Attributes;
+import io.opentelemetry.api.trace.SpanKind;
+import io.opentelemetry.context.Context;
+import io.opentelemetry.sdk.trace.data.LinkData;
 import io.opentelemetry.sdk.trace.samplers.Sampler;
+import io.opentelemetry.sdk.trace.samplers.SamplingResult;
 import lombok.extern.log4j.Log4j2;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.boot.actuate.autoconfigure.tracing.TracingProperties;
 import org.springframework.boot.actuate.autoconfigure.tracing.otlp.OtlpGrpcSpanExporterBuilderCustomizer;
 import org.springframework.context.annotation.*;
 import org.springframework.core.env.Environment;
 import org.springframework.core.type.AnnotatedTypeMetadata;
+
+import java.util.List;
 
 /**
  * @see org.springframework.boot.actuate.autoconfigure.tracing.OpenTelemetryAutoConfiguration
@@ -36,6 +45,7 @@ public class CustomizedOtelConfiguration {
 
 
     @Bean
+    @Conditional(CustomizedOtelEnabledCondition.class)
     public OtlpGrpcSpanExporterBuilderCustomizer customizedOtelTracingExporterBuilderCustomizer() {
         String appenv = System.getenv(APP_ENV);
         String tracingEndpoint = System.getenv(TRACING_ENDPOINT);
@@ -46,15 +56,33 @@ public class CustomizedOtelConfiguration {
                 .addHeader("X-Scope-OrgID", appenv);
     }
 
-
-
-
     @Bean
     @Primary
     @Conditional(CustomizedOtelEnabledCondition.class)
-    Sampler customizedOtelSampler(Environment environment) {
+    Sampler customizedOtelSampler(Environment environment, TracingProperties properties) {
         String tracingSampleRatio = environment.getProperty(TRACING_SAMPLE_RATIO);
-        Sampler rootSampler = Sampler.traceIdRatioBased(Double.parseDouble(tracingSampleRatio));
+        double ratio;
+        if (StringUtils.isBlank(tracingSampleRatio)) {
+            ratio = properties.getSampling().getProbability();
+        }else {
+            //测试环境支持全部drop的
+            ratio = Double.parseDouble(tracingSampleRatio);
+            if (ratio <= 0) {
+                return new Sampler() {
+                    @Override
+                    public SamplingResult shouldSample(Context parentContext, String traceId, String name, SpanKind spanKind, Attributes attributes, List<LinkData> parentLinks) {
+                        return SamplingResult.drop();
+                    }
+
+                    @Override
+                    public String getDescription() {
+                        return "NoopSampler";
+                    }
+                };
+            }
+        }
+
+        Sampler rootSampler = Sampler.traceIdRatioBased(ratio);
         log.info("init Sampler with tracingSampleRatio: {}", tracingSampleRatio);
         return Sampler.parentBased(rootSampler);
     }
