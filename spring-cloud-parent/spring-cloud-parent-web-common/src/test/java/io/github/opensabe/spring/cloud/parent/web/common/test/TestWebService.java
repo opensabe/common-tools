500b13f05fcb9c85a9264be7e0e03a7b444368c0
opensabe-tech <tech@sabegeek.com>
2026-03-20 17:24:42 +0800

    feat(test): 升级 testcontainers
    (cherry picked from commit 806d4ddf8e40b6030834f39c95f2e7c20a8001ec)

--- spring-cloud-parent/spring-cloud-parent-web-common/src/test/java/io/github/opensabe/spring/cloud/parent/web/common/test/TestWebService.java
+++ spring-cloud-parent/spring-cloud-parent-web-common/src/test/java/io/github/opensabe/spring/cloud/parent/web/common/test/TestWebService.java
@@ -8,6 +8,8 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.extern.log4j.Log4j2;
+
+import org.apache.commons.lang3.StringUtils;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -23,7 +25,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.context.request.async.DeferredResult;
-import org.testcontainers.shaded.org.apache.commons.lang3.StringUtils;
 import reactor.core.publisher.Mono;
 
 import java.math.BigDecimal;
